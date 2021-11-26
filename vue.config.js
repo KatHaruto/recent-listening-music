fc276c5c728aeb48030147abb0ae14c40cc27961
tr-author <katsushiro@g.ecc.u-tokyo.ac.jp>
2021-11-26 17:33:37 +0900

    change public path for prod env

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,6 @@
-const isProd = process.env.NODE_ENV === "production";
-
 module.exports = {
   transpileDependencies: ["vuetify"],
-  publicPath: isProd ? "/recent-listening-music" : "/",
+  publicPath: "./",
   outputDir: "docs",
   filenameHashing: false,
   productionSourceMap: false,
